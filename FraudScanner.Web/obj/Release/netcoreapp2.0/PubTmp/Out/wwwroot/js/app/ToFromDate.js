350dee6cd5e9efe719625abcff6f820267450b48
Chaimspear <chaimspear@gmail.com>
2018-02-21 15:39:54 -0500

    First Commit for project to Github

---
+++ FraudScanner.Web/obj/Release/netcoreapp2.0/PubTmp/Out/wwwroot/js/app/ToFromDate.js
@@ -0,0 +1,34 @@
+﻿$(document).ready(function () {
+    alert("test 12")
+    set_dates();
+}
+);
+set_dates = function () {
+
+    $("#FromDate").datepicker({
+        changeMonth: true,
+        changeYear: true,
+        dateFormat: 'mm/dd/yy',
+        gotoCurrent: true
+    });
+    $("#FromDate").attr('maxlength', '10');
+    $("#FromDate").val("@ViewData["fromdate"].ToString()");
+
+    $("#ToDate").datepicker({
+        changeMonth: true,
+        changeYear: true,
+        dateFormat: 'mm/dd/yy',
+        gotoCurrent: true
+    });
+    $("#ToDate").attr('maxlength', '10');
+    $("#ToDate").val("@ViewData["todate"].ToString()");
+}
+
+ToFromDate = {
+    get_from_date_val: function () {
+        return $("#FROM_DATE").val();
+    },
+    get_to_date_val: function () {
+        return $("#TO_DATE").val();
+    }
+}
